93516c99892c3937af78ef9315abdd3db3a858f6
LeDernierDeNous <mouloud3826@gmail.com>
2024-08-09 17:50:19 +0200

    Update test_mine_classes.py

--- tests/test_mine_classes.py
+++ tests/test_mine_classes.py
@@ -1,27 +1,44 @@
 import unittest
 from src.buildings.mine import MetalMine, CrystalField, DeuteriumSynthesizer
+from src.resources.resourcetype import ResourceType
 
+class TestMineBuildings(unittest.TestCase):
 
-class TestMineClasses(unittest.TestCase):
-
-    def test_metal_mine(self):
+    def test_metal_mine_initialization(self):
         metal_mine = MetalMine()
-        self.assertEqual(metal_mine.get_production(), 0)  # Add appropriate test value
-        upgrade_cost = metal_mine.calculate_upgrade_cost()
-        # Add appropriate assertions for upgrade cost
+        self.assertEqual(metal_mine.get_name(), "Metal Mine")
+        self.assertEqual(metal_mine.get_type(), "metalmine")
+        self.assertEqual(metal_mine.resource_type, ResourceType.METAL)
 
-    def test_crystal_field(self):
+    def test_crystal_field_initialization(self):
         crystal_field = CrystalField()
-        self.assertEqual(crystal_field.get_production(), 0)  # Add appropriate test value
-        upgrade_cost = crystal_field.calculate_upgrade_cost()
-        # Add appropriate assertions for upgrade cost
+        self.assertEqual(crystal_field.get_name(), "Crystal Mine")
+        self.assertEqual(crystal_field.get_type(), "crystalfield")
+        self.assertEqual(crystal_field.resource_type, ResourceType.CRYSTAL)
 
-    def test_deuterium_synthesizer(self):
+    def test_deuterium_synthesizer_initialization(self):
         deuterium_synthesizer = DeuteriumSynthesizer()
-        self.assertEqual(deuterium_synthesizer.get_production(), 0)  # Add appropriate test value
-        upgrade_cost = deuterium_synthesizer.calculate_upgrade_cost()
-        # Add appropriate assertions for upgrade cost
+        self.assertEqual(deuterium_synthesizer.get_name(), "Deuterium Mine")
+        self.assertEqual(deuterium_synthesizer.get_type(), "deuteriumsynthesizer")
+        self.assertEqual(deuterium_synthesizer.resource_type, ResourceType.DEUTERIUM)
 
+    def test_metal_mine_production(self):
+        metal_mine = MetalMine()
+        metal_mine.level = 5
+        expected_production = int(30 * 5 * (1.1 ** 5))
+        self.assertEqual(metal_mine.get_production(), expected_production)
 
-if __name__ == '__main__':
+    def test_upgrade_costs(self):
+        metal_mine = MetalMine()
+        metal_mine.level = 1
+        costs = metal_mine.calculate_upgrade_cost()
+        self.assertEqual(costs[ResourceType.METAL], 60)
+        self.assertEqual(costs[ResourceType.CRYSTAL], 15)
+
+    def test_static_types(self):
+        self.assertEqual(MetalMine.get_static_type(), "metalmine")
+        self.assertEqual(CrystalField.get_static_type(), "crystalfield")
+        self.assertEqual(DeuteriumSynthesizer.get_static_type(), "deuteriumsynthesizer")
+
+if __name__ == "__main__":
     unittest.main()
